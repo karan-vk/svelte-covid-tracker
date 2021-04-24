4618d7cfab4c6d5bbc7fe968c1289763742a8ffc
karan-vk <karanvknarayanan@gmail.com>
2021-04-24 14:00:29 +0530

    animations added

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   import CountrySelect from "./Components/CountrySelect.svelte";
   import DataBlocks from "./Components/DataBlocks.svelte";
   import DataTile from "./Components/DataTile.svelte";
+  import { fade } from "svelte/transition";
+  import { cubicIn } from "svelte/easing";
 
   import Header from "./Components/Header.svelte";
   import Loading from "./Components/Loading.svelte";
@@ -21,6 +23,7 @@
     const data = await fetchCovidData();
     dataDate = data.Date;
     stats = data.Global;
+    title = "Global";
     countries = data.Countries;
     isLoading = false;
   };
@@ -36,18 +39,20 @@
   {#if isLoading}
     <Loading />
   {:else}
-    <DataTile {dataDate} {title} />
-    <DataBlocks {stats} />
-    <CountrySelect {countries} on:get-country={changeCountry} />
-    {#if stats.Country}
-      <button
-        on:click={reset}
-        v-if="stats.Country"
-        class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-500"
-      >
-        Clear country
-      </button>
-    {/if}
+    <div transition:fade={{ easing: cubicIn }}>
+      <DataTile {dataDate} {title} />
+      <DataBlocks {stats} />
+      <CountrySelect {countries} on:get-country={changeCountry} />
+      {#if stats.Country}
+        <button
+          on:click={reset}
+          v-if="stats.Country"
+          class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-500"
+        >
+          Clear country
+        </button>
+      {/if}
+    </div>
   {/if}
 </div>
 
